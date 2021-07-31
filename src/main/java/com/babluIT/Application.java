0e87bd456e453a8449feeb22ad0ca15b9d593943
BabluIT <iasbablu1998@gmail.com>
2021-07-31 20:46:22 +0530

    My first eclipse commit

--- src/main/java/com/babluIT/Application.java
+++ src/main/java/com/babluIT/Application.java
@@ -3,11 +3,10 @@ package com.babluIT;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class Application {
 
-	int a=10;int b=20;c=50;
+	int a = 10;int b=20;c=50;d=20;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
